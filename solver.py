eeff0611b9b5806bfd711c07bb42ee2ad580c317
Isaac <isaaclaw12@gmail.com>
2019-08-09 23:21:40 -0500

    Begin solver class. Create block interaction functions. Overlap, contains, etc.

--- solver.py
+++ solver.py
@@ -9,10 +9,13 @@ class Block:
     def __repr__(self):
         return str(self.min_corner) + str(self.max_corner) + "\n"
 
+    def dimensions(self):
+        self.max_corner - self.min_corner
+
     def hash_corners(self):
         return hash((self.min_corner, self.max_corner))
 
-    def include_coordinates(self, position):
+    def append_coordinate(self, position):
         # Expand the block to contain the coordinates specified by position
         compare = np.array([self.min_corner, position])
         self.min_corner = np.min(compare, axis=0)
@@ -29,11 +32,44 @@ class Block:
                 indexes.append(index)
         return indexes
 
+    def is_adjacent(self, block):
+        # Blocks share sides, but do not overlap
+        x_overlap, y_overlap = self.xy_overlaps(block)
+        shared_edge = 0 in [x_overlap, y_overlap] and x_overlap + y_overlap > 0
+        return shared_edge
+
+    def does_intersect(self, block):
+        # Blocks share some area
+        x_overlap, y_overlap = self.xy_overlaps(block)
+        x_overlap = max(x_overlap, 0)
+        y_overlap = max(y_overlap, 0)
+        return x_overlap * y_overlap > 0
+
+    def contains(self, block):
+        x_overlap, y_overlap = self.xy_overlaps(block)
+        return block.dimensions() == np.array([x_overlap, y_overlap])
+
+    def xy_overlaps(self, block):
+        # Calculates the amount the blocks overlap in the x and y directions
+        # Negative values represent distance between blocks
+        x_overlap = self.block_overlap(block, axis=0)
+        y_overlap = self.block_overlap(block, axis=1)
+        return x_overlap, y_overlap
+
+    def block_overlap(self, block, axis):
+        # Returns the overlap between the current block and block, along axis
+        overlap = min(self.max_corner[axis], block.max_corner[axis]) - max(self.min_corner[axis], block.min_corner[axis])
+        return overlap
+
+
 class GameBoard:
+    self.PLAYER_CHAR = "*"
+    self.GOAL_CHAR = "_"
     def __init__(self, filename):
         self.filename = filename
         self.shape = (5, 4)
         self.blocks = {}
+        self.goal_block = None
         self.load_game()
 
     def hash_board_state(self):
@@ -41,7 +77,7 @@ class GameBoard:
         # hash value
         block_hashes = []
         for key in self.blocks:
-            block_hashes.append(hash(self.blocks[key]))
+            block_hashes.append(self.blocks[key].hash_corners())
         block_hashes.sort()
         return hash(tuple(block_hashes))
 
@@ -64,9 +100,12 @@ class GameBoard:
             for row in range(1, self.shape[0]+1):
                 for col in range(self.shape[1]):
                     current = lines[row][col]
+                    # Leave empty spots empty
+                    if current == self.GOAL_CHAR:
+                        continue
                     position = [row-1, col]
                     if current in self.blocks:
-                        self.blocks[current].include_coordinates(position)
+                        self.blocks[current].append_coordinate(position)
                     else:
                         self.blocks[current] = Block(position)
         except IndexError:
@@ -79,9 +118,39 @@ class GameBoard:
         self.blocks = blocks
 
     def available_moves(self):
-        # Find empty blocks
+        empty_blocks = self.empty_blocks()
         # Iterate through blocks, find if they are adjacent to empty blocks
-        # Check whether the move can be made without collisons
+        adjacent_obstacles = []
+        for key in self.blocks:
+            obstacle = self.blocks(key)
+            for empty_block in empty_blocks:
+                if obstacle.is_adjacent(empty_block):
+                    adjacent_obstacles.append(keys)
+        # Check whether a move can be made without collisons
+        return self.valid_moves(adjacent_obstacles)
+
+    def empty_blocks(self):
+        # Returns a list of the 1x1 blocks that are empty
+        board = np.zeros(self.shape)
+        for letter in self.blocks:
+            for index in self.blocks[letter].covered_indexes():
+                board[tuple(index)] = 1
+        empty_blocks = []
+        for row in range(board.shape[0]):
+            for col in range(board.shape[1]):
+                position = [row, col]
+                if not board[position]:
+                    empty_blocks.append(Block(position))
+        return empty_blocks
+
+    def valid_moves(self, obstacle_keys):
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for move in moves:
+            for obstacle_k in obstacle_keys:
+
+    def is_valid_move(self, block_key, move):
+
+
 
     def print_board(self):
         board = np.chararray(self.shape)
