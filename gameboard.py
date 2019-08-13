0eed181048a0c904071836e4ff27cc4a554e94c5
Isaac <isaaclaw12@gmail.com>
2019-08-13 13:40:02 -0500

    Basic solving functionality in place

--- gameboard.py
+++ gameboard.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 from block import Block
 
 class GameBoard:
@@ -35,6 +36,10 @@ class GameBoard:
                     lines.append(line)
         return lines
 
+    def set_shape(self, shape):
+        self.shape = shape
+        self.board = Block((0,0), self.shape)
+
     def load_file(self, lines):
         try:
             self.shape = [int(x) for x in lines[0].split()]
@@ -53,29 +58,26 @@ class GameBoard:
             print("Invalid game format")
 
     def get_state(self):
-        return self.blocks
+        return deepcopy(self.blocks)
 
     def set_state(self, blocks):
-        self.blocks = blocks
+        self.blocks = deepcopy(blocks)
 
     def available_moves(self):
         empty_blocks = self.empty_blocks()
         # Iterate through blocks, find if they are adjacent to empty blocks
-        adjacent_obstacles = []
+        possible_obstacles = []
         for key in self.blocks:
             obstacle = self.blocks(key)
             for empty_block in empty_blocks:
                 if obstacle.is_adjacent(empty_block):
-                    adjacent_obstacles.append(keys)
+                    possible_obstacles.append(keys)
         # Check whether a move can be made without collisons
-        return self.valid_moves(adjacent_obstacles)
+        return self.valid_move_states(possible_obstacles)
 
     def empty_blocks(self):
         # Returns a list of the 1x1 blocks that are empty
-        board = np.zeros(self.shape)
-        for letter in self.blocks:
-            for index in self.blocks[letter].covered_indexes():
-                board[tuple(index)] = 1
+        board, _ = self.board_coverage()
         empty_blocks = []
         for row in range(board.shape[0]):
             for col in range(board.shape[1]):
@@ -84,14 +86,44 @@ class GameBoard:
                     empty_blocks.append(Block(position))
         return empty_blocks
 
-    def valid_moves(self, obstacle_keys):
+    def board_coverage(self):
+        # Returns an array in the shape of board, each index contains a count of
+        # blocks containing it
+        board = np.zeros(self.shape)
+        blocks_out_of_bounds = False
+        for letter in self.blocks:
+            for index in self.blocks[letter].covered_indexes():
+                if not self.index_in_bounds(index):
+                    blocks_out_of_bounds = True
+                    continue
+                board[tuple(index)] += 1
+        return board, blocks_out_of_bounds
+
+    def index_in_bounds(self, index):
+        in_bounds = False
+        if index[0] >=0 and index[0] < self.shape[0]:
+            if index[1] >=0 and index[1] < self.shape[1]:
+                in_bounds = True
+        return in_bounds
+
+    def valid_move_states(self, obstacle_keys):
+        # Attempt to move each passed obstacle in all four directions
         moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        valid_states = []
         for move in moves:
             for obstacle_k in obstacle_keys:
-                pass
+                is_valid, move_state = self.is_valid_state(obstacle_k, move)
+                if is_valid:
+                    valid_move_states.append(move_state)
+        return valid_states
 
-    def is_valid_move(self, block_key, move):
-        pass
+    def is_valid_state(self, block_key, move):
+        self.blocks[block_key].move_block(move)
+        move_state = self.get_state()
+        board_coverage, blocks_out_of_bounds = self.board_coverage()
+        self.blocks[block_key].move_block(move*-1)
+        is_valid = (board_coverage <= 1).all() and not blocks_out_of_bounds
+        return is_valid, move_state
 
     def solved(self):
         return False
